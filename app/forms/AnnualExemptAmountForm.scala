abba877c27132f287d7397089da70c5926d9a0f7
james.forster <james.forster@digital.hmrc.gov.uk>
2016-05-10 16:12:59 +0100

    Fixed messages where broken or inconsistent.

--- app/forms/AnnualExemptAmountForm.scala
+++ app/forms/AnnualExemptAmountForm.scala
@@ -38,8 +38,8 @@ object AnnualExemptAmountForm {
 
   def errorMaxMessage(isAllowedMaxAEA: Boolean): String = {
     isAllowedMaxAEA match {
-      case true => Messages("calc.annualExemptAmount.errorMax") + maxAEA
-      case false => Messages("calc.annualExemptAmount.errorMax") + maxNonVulnerableTrusteeAEA
+      case true => Messages("calc.annualExemptAmount.errorMax") + maxAEA +  Messages("calc.annualExemptAmount.errorMaxEnd")
+      case false => Messages("calc.annualExemptAmount.errorMax") + maxNonVulnerableTrusteeAEA + Messages("calc.annualExemptAmount.errorMaxEnd")
     }
   }
 
